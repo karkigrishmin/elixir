589042e3546c455fbcfa6d388fe4dbd9ef8b47a3
karkigrishmin <grishmink@gmail.com>
2021-05-13 17:08:46 +0545

    day2:read about variables, modules, functions from elixir in action book

--- test.ex
+++ test.ex
@@ -8,7 +8,13 @@ defmodule M do
   def data_stuff do
     # my_int = 16
     # IO.puts("Integer #{is_integer(my_int)}")
-    my_float = 2.13
-    IO.puts("Float #{is_float(my_float)}")
+
+    # my_float = 2.13
+    # IO.puts("Float #{is_float(my_float)}")
+
+    # IO.puts("Atom #{is_atom(:harry)}")
+
+    # ranges from 1 to 10
+    # one_to_ten = 1..10
   end
 end
