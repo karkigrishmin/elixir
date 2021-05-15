d6a45919153d147e1a3e0d5cf28cb1ad53672512
karkigrishmin <grishmink@gmail.com>
2021-05-15 19:11:50 +0545

    day4:read about typesystems and implemented those concepts

--- ch_2/sample_1.ex
+++ ch_2/sample_1.ex
@@ -83,3 +83,98 @@ defmodule AliasExample do
     MyIO.puts("giving another name for IO as MyIO")
   end
 end
+
+# type system
+# Numbers, atoms, atoms as booleans
+defmodule NumbersExample do
+  def sum do
+    a = 5
+    b = 2
+    a + b
+  end
+
+  def div_example do
+    a = 7
+    b = 3
+    # returns integer value ie.2, but 7/3 gives float value i.e. 2.5
+    div(a, b)
+  end
+
+  def remainder_example do
+    # returns remainder i.e 1
+    rem(7, 3)
+  end
+end
+
+# atoms
+# atoms consists of two parts- text and value
+# text is the character after colon and text is stored in atom table at runtime
+# value is the reference of the atom table that is stored in the variable
+defmodule AtomsExample do
+  def print_atom do
+    a = :an_atom
+    # we can use this syntax for spaces in our atom name
+    b = :"an atom with spaces"
+    # a
+    b
+  end
+end
+
+# aliases-another syntax for atom constants
+defmodule AnotherAtomSyntax do
+  def another_syntax_for_atom do
+    a = AnAtom
+    # at compile time it's transformed into :"Elixir.AnAtom"
+    a == :"Elixir.AnAtom"
+  end
+
+  def test_atom do
+    a = AnAtom
+    # when we use alias, elixir implicitly adds Elixir. prefix to its text and
+    # inserts and atom there
+    a == Elixir.AnAtom
+  end
+end
+
+# atoms as booleans
+# there is no dedicated boolean type in elixir so we use :true and :false atoms
+# as booleans
+# elixir also allows us to reference these atoms without colon- true and false
+defmodule BooleanAtomsExample do
+  def example_1 do
+    a = true
+    b = false
+    # returns true
+    a
+    # returns false
+    b
+  end
+
+  def example_2 do
+    # returns false
+    true and false
+    # returns true
+    true or false
+    # returns false
+    not true
+  end
+end
+
+# Nil
+# another atom i.e :nil or nil
+# elixirs short circuit operators- ||, &&, and !
+defmodule ShortCircuitOperatorExample do
+  def example_1 do
+    # returns first expression that is not falsy value, so here it returns 5
+    nil || 5 || false || true
+    # returns last expression if there are no any truthy value
+    nil || false || false
+  end
+
+  def example_2 do
+    # returns first expression if it is not truthy
+    nil && true
+    # returns second expression if the first expression is truthy
+    true && false
+  end
+end
