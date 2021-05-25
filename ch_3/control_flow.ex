ad702a273067b9ea7d1a4da752ef3e35b87c4fca
karkigrishmin <grishmink@gmail.com>
2021-05-25 18:03:51 +0545

    day13:read about matching constants, variables in patterns, matching lists and matching maps and did some examples

--- ch_3/control_flow.ex
+++ ch_3/control_flow.ex
@@ -23,6 +23,10 @@ defmodule MatchOperatorExample do
     {name, age} = {"Harry", 20}
     name
     age
+
+    # the match expression also returns a value
+    # and that value is the right side term that we're matching against
+    {name, age} = {"Harry", 22}
   end
 
   def return_tuple_from_function do
@@ -38,5 +42,98 @@ defmodule MatchOperatorExample do
     {hour, minute, second} = time
     year
     hour
+  end
+
+  def matching_constants do
+    # we can also put constants on the leftside of the match expression
+    # here pattern 1 matches to the term 1 and
+    # the result of the entire expression is the right-side term
+    1 = 1
+
+    person = {:person, "John", 19}
+    # here we expect the right-side term to be a three-element tuple
+    # after the match, name and age are bound to the corresponding elements of tuple
+    # in the right-side term
+    {:person, name, age} = person
+    name
+    age
+
+    # many functions from elixir or erlang return either {:ok, result}
+    # or {:error, reason}
+    # we can read the file contents with the help of File.read/1 function
+    # so here we are attempting to open and read the file
+    # if it succeeds, the file contents are extracted to the variable contents
+    # if it fails, an error is raised
+    # because the result of File.read() is a tuple in the form {:error, reason}
+    # so the match to {:ok, contents} fails
+    {:ok, contents} = File.read("../test.ex")
+  end
+
+  def variables_in_patterns do
+    # varible name in the left-side pattern
+    # always matches to the corresponding right-side pattern
+    # and the variable is bound to the value of the term
+    # if we dont want the value from the right-side term
+    # but we still need to match on it, in this case
+    # we can use the anonymous variable(_)
+    # in the following example, we only need time, we specify time variable to store it
+    # the value of the term isn't bound to anonymous variable
+    {_, time} = :calendar.local_time()
+    time
+
+    # we can also add a descriptive name after undersccore character
+    # _date is regarded as an anonymous variable
+    {_date, time} = :calendar.local_time()
+    _date
+
+    # patterns can be arbitrarily nested
+    # we can only retrieve the current hour of a day
+    {_, {hour, _, _}} = :calendar.local_time()
+    hour
+
+    # a  variable can be referenced multiple times in the same pattern.
+    # matches a tuple with three identical elements
+    {amount, amount, amount} = {127, 127, 127}
+    # fails because the tuple elements aren't identical
+    # {amount, amount, amount} = {127, 127, 1}
+
+    # if we need to match against the contents of the variable
+    # for this purpose, we use pin operator(^)
+    expected_name = "Harry"
+    {^expected_name, _} = {"Harry", 20}
+    # notice that, pin operator(^) doesn't bind the variable
+  end
+
+  def matching_lists do
+    # list matching works similarly to tuples
+    [first, second, third] = [1, 2, 3]
+    # the previously mentioned pattern techniques work as well:
+    [1, second, third] = [1, 2, 3]
+
+    [first, _, third] = [1, 2, 3]
+
+    first = 1
+    [^first, second, third] = [1, 2, 3]
+
+    [first, first, first] = [1, 1, 1]
+
+    # matching list is more often done relying on their recursive nature
+    [head | tail] = [1, 2, 3]
+    head
+    tail
+
+    # if we only need one element of the [head, tail] pair, we can use hd function
+    hd([1, 2, 3])
+    # the pattern [head | _] is more useful when pattern-matching function arguments
+  end
+
+  def matching_maps do
+    # to match a map, the following syntax can be used
+    %{name: name, age: age} = %{name: "Harry", age: 20}
+    name
+    age
+    # the left-side pattern doesn't need to contain all the keys from the right-side term
+    %{age: age} = %{name: "Harry", age: 20}
+    age
   end
 end
